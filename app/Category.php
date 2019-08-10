fb1dbf9ccdcac0a7035d12a56a3c37e7ee670064
bretton <867057410@qq.com>
2019-08-10 13:30:24 +0800

    排序问题

--- app/Category.php
+++ app/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
 
     public function children()
     {
-        return $this->hasMany(static::class, 'parent_id');
+        return $this->hasMany(static::class, 'parent_id')->orderBy('order');
     }
 
     public function sites()
